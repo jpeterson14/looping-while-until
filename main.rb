7f3bca2a0c33ca08f957f4aa3c2567f40e710ad3
jpeterson0514 <>
2021-05-06 20:13:31 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,14 @@
+puts "While Loop:"
+counter = 0
+while counter < 5
+  puts "The current counter is less than 5."
+  counter += 1
+end
+
+
+puts "Until Loop:"
+token = 0
+until token == 7
+  puts "The current token is less than 7."
+  token += 1
+end
